e0f9490ec872b362b2a069c2c240e8663e7e8377
devaraujstein <andreearaujo98@gmail.com>
2022-09-04 03:19:48 -0300

    finalizing challenge

--- node/nodejs/src/index.js
+++ node/nodejs/src/index.js
@@ -1,13 +1,70 @@
 const express = require('express');
-
+const mysql = require('mysql');
 const app = express();
 
-const response = `
-</p>
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'passroot',
+    database: 'nodedb'
+};
+
+const connection = mysql.createConnection(config);
+
+connection.connect();
+
+connection.query(`
+    CREATE TABLE IF NOT EXISTS 
+        people (
+            id int not null auto_increment primary key, 
+            name varchar(255)
+        )`
+);
+
+connection.query(`
+    INSERT INTO 
+        people(name) 
+    VALUES 
+        ('André'),
+        ('Fulano'),
+        ('Ciclano'),
+        ('Beltrano')
+`);
+
+connection.query(`
+    SELECT 
+        * 
+    FROM
+        people
+`, (error, rows, fields) => {
+    if (error) throw error;
+    getPeople(rows);
+});
+
+connection.end();
+
+let response = `
     <p> <h1> Full Cycle Rocks! </h1> </p>
     <p> - Lista de nomes cadastrada no banco de dados - </p>
-<p> 
+    </br>
+    <table style="border: 1px solid black;">
+    <tr>
+      <th> id </th>
+      <th> name </th>
+    </tr>
 `;
+
+const getPeople = (rows) =>{
+    rows.forEach(row => {
+        response += `
+            <tr>
+                <td>${row.id}</td>
+                <td>${row.name}</td>
+            </tr>
+        `;
+    });
+    response += '</table>'
+}
 
 app.get('/', (req, res) => {
     res.send(response)
